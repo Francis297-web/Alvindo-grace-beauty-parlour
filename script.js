cc3d264f2f0acb1a7e2e34024a38aecb6f86c099
Francis297-web <francischege381@gmail.com>
2026-06-24 02:16:31 -0400

    Add smooth scrolling and navbar background change on scroll

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", () => {
+
+const nav = document.querySelector("nav");
+
+if(window.scrollY > 50){
+nav.style.background = "#000";
+}else{
+nav.style.background = "transparent";
+}
+
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener('click', function(e){
+
+e.preventDefault();
+
+document.querySelector(
+this.getAttribute('href')
+).scrollIntoView({
+behavior:'smooth'
+});
+
+});
+
+});
